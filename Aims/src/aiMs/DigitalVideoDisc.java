51341b01e05a05283e87e606cbb3888b18694f1f
Dinhthanh05 <dinhthanh020105@gmail.com>
2024-11-26 22:04:26 +0700

    lab3

---
+++ Aims/src/aiMs/DigitalVideoDisc.java
@@ -0,0 +1,24 @@
+package aiMs;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length; 
+    private float cost; 
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String ss) { this.title = ss; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
+}
