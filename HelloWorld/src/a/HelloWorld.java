165489ceceffd3afad526ae15be346cf7d17a70f
Dinhthanh05 <dinhthanh020105@gmail.com>
2024-12-03 05:32:09 +0700

    Implement Book and CompactDisc classes

---
+++ HelloWorld/src/a/HelloWorld.java
@@ -0,0 +1,10 @@
+public class Person {
+    // Thuộc tính (biến) của lớp Person
+    String name;
+    int age;
+
+    // Phương thức của lớp Person
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
---
+++ hocoban/src/hocoban/Person.java
@@ -0,0 +1,13 @@
+package hocoban;
+
+public class Person {
+    // Thuộc tính (biến) của lớp Person
+    String name;
+    int age;
+
+    // Phương thức của lớp Person
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
+
